647d8e1bd5cde8f63f903d41db08fffdf8d5d786
Erick Santos <ericksantosdantas@gmail.com>
2025-07-11 09:22:56 -0300

    Update nota_servico.py

--- app/schemas/nota_servico.py
+++ app/schemas/nota_servico.py
@@ -4,50 +4,50 @@ from decimal import Decimal
 from datetime import date
 
 class NotaServicoBase(BaseModel):
-    tipo_de_registro: Optional[str]
-    numero_nfse: Optional[int]
-    status_nfse: Optional[Decimal]
-    codigo_verificacao: Optional[str]
-    data_emissao: Optional[date]
-    tipo_rps: Optional[Decimal]
-    serie_rps: Optional[Decimal]
-    numero_rps: Optional[Decimal]
-    data_emissao_rps: Optional[str]
+    tipo_de_registro: Optional[str] = None
+    numero_nfse: Optional[int] = None
+    status_nfse: Optional[Decimal] = None
+    codigo_verificacao: Optional[str] = None
+    data_emissao: Optional[date] = None
+    tipo_rps: Optional[Decimal] = None
+    serie_rps: Optional[Decimal] = None
+    numero_rps: Optional[Decimal] = None
+    data_emissao_rps: Optional[str] = None
 
-    cpf_cnpj_prestador: Optional[str]
-    inscricao_municipal_prestador: Optional[str]
-    razao_social_prestador: Optional[str]
-    endereco_prestador: Optional[str]
-    cidade_prestador: Optional[str]
-    uf_prestador: Optional[str]
-    email_prestador: Optional[str]
+    cpf_cnpj_prestador: Optional[str] = None
+    inscricao_municipal_prestador: Optional[str] = None
+    razao_social_prestador: Optional[str] = None
+    endereco_prestador: Optional[str] = None
+    cidade_prestador: Optional[str] = None
+    uf_prestador: Optional[str] = None
+    email_prestador: Optional[str] = None
 
-    cpf_cnpj_tomador: Optional[str]
-    inscricao_municipal_tomador: Optional[str]
-    razao_social_tomador: Optional[str]
-    endereco_tomador: Optional[str]
-    cidade_tomador: Optional[str]
-    uf_tomador: Optional[str]
-    email_tomador: Optional[str]
+    cpf_cnpj_tomador: Optional[str] = None
+    inscricao_municipal_tomador: Optional[str] = None
+    razao_social_tomador: Optional[str] = None
+    endereco_tomador: Optional[str] = None
+    cidade_tomador: Optional[str] = None
+    uf_tomador: Optional[str] = None
+    email_tomador: Optional[str] = None
 
-    tipo_tributacao: Optional[Decimal]
-    cidade_servico: Optional[str]
-    uf_servico: Optional[str]
-    regime_especial: Optional[Decimal]
-    simples_nacional: Optional[Decimal]
-    incentivo_cultural: Optional[Decimal]
-    codigo_atividade_municipal: Optional[Decimal]
+    tipo_tributacao: Optional[Decimal] = None
+    cidade_servico: Optional[str] = None
+    uf_servico: Optional[str] = None
+    regime_especial: Optional[Decimal] = None
+    simples_nacional: Optional[Decimal] = None
+    incentivo_cultural: Optional[Decimal] = None
+    codigo_atividade_municipal: Optional[Decimal] = None
 
-    aliquota: Optional[str]
-    valor_servico: Optional[str]
-    valor_deducoes: Optional[str]
-    valor_iss: Optional[str]
-    valor_outros: Optional[str]
-    iss_retido: Optional[Decimal]
+    aliquota: Optional[str] = None
+    valor_servico: Optional[str] = None
+    valor_deducoes: Optional[str] = None
+    valor_iss: Optional[str] = None
+    valor_outros: Optional[str] = None
+    iss_retido: Optional[Decimal] = None
 
-    discriminacao_servico: Optional[str]
-    competencia: Optional[str]
-    valor_total_recebido: Optional[str]
+    discriminacao_servico: Optional[str] = None
+    competencia: Optional[str] = None
+    valor_total_recebido: Optional[str] = None
 
     model_config = {
         "from_attributes": True
