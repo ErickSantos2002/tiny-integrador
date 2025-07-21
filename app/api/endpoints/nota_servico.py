f31d42fc18dff8f18b9edd86fb58a91dfabf8be5
Erick Santos <ericksantosdantas@gmail.com>
2025-07-21 14:39:13 -0300

    Update nota_servico.py

--- app/api/endpoints/nota_servico.py
+++ app/api/endpoints/nota_servico.py
@@ -5,6 +5,8 @@ from datetime import date
 from app.models.database import SessionLocal
 from app.models.nota_servico import NotaServico as NotaServicoModel
 from app.schemas.nota_servico import NotaServico
+from fastapi.responses import JSONResponse
+import traceback
 
 router = APIRouter(prefix="/notas_servico", tags=["Notas de Serviço"])
 
@@ -27,27 +29,36 @@ def listar_notas_servico(
     uf_servico: Optional[str] = Query(None),
     db: Session = Depends(get_db)
 ):
-    query = db.query(NotaServicoModel)
+    try:
+        query = db.query(NotaServicoModel)
 
-    if cpf_cnpj_tomador:
-        query = query.filter(NotaServicoModel.cpf_cnpj_tomador.ilike(f"%{cpf_cnpj_tomador}%"))
+        if cpf_cnpj_tomador:
+            query = query.filter(NotaServicoModel.cpf_cnpj_tomador.ilike(f"%{cpf_cnpj_tomador}%"))
 
-    if cpf_cnpj_prestador:
-        query = query.filter(NotaServicoModel.cpf_cnpj_prestador.ilike(f"%{cpf_cnpj_prestador}%"))
+        if cpf_cnpj_prestador:
+            query = query.filter(NotaServicoModel.cpf_cnpj_prestador.ilike(f"%{cpf_cnpj_prestador}%"))
 
-    if data_inicio and data_fim:
-        query = query.filter(NotaServicoModel.data_emissao.between(data_inicio, data_fim))
-    elif data_inicio:
-        query = query.filter(NotaServicoModel.data_emissao >= data_inicio)
-    elif data_fim:
-        query = query.filter(NotaServicoModel.data_emissao <= data_fim)
-    elif data_emissao:
-        query = query.filter(NotaServicoModel.data_emissao == data_emissao)
+        if data_inicio and data_fim:
+            query = query.filter(NotaServicoModel.data_emissao.between(data_inicio, data_fim))
+        elif data_inicio:
+            query = query.filter(NotaServicoModel.data_emissao >= data_inicio)
+        elif data_fim:
+            query = query.filter(NotaServicoModel.data_emissao <= data_fim)
+        elif data_emissao:
+            query = query.filter(NotaServicoModel.data_emissao == data_emissao)
 
-    if cidade_servico:
-        query = query.filter(NotaServicoModel.cidade_servico.ilike(f"%{cidade_servico}%"))
+        if cidade_servico:
+            query = query.filter(NotaServicoModel.cidade_servico.ilike(f"%{cidade_servico}%"))
 
-    if uf_servico:
-        query = query.filter(NotaServicoModel.uf_servico == uf_servico)
+        if uf_servico:
+            query = query.filter(NotaServicoModel.uf_servico == uf_servico)
 
-    return query.all()
+        return query.all()
+
+    except Exception as e:
+        print("ERRO NA API:")
+        traceback.print_exc()
+        return JSONResponse(
+            status_code=500,
+            content={"erro": str(e)}
+        )
